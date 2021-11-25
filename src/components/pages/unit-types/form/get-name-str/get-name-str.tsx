3fc38c632db90c6c0bcf9532af56677b8041ca0e
PavelMGs <pavelmg99@gmail.com>
2021-11-25 17:58:42 +0200

    name

--- src/components/pages/unit-types/form/get-name-str/get-name-str.tsx
+++ src/components/pages/unit-types/form/get-name-str/get-name-str.tsx
@@ -1,11 +1,18 @@
+import CodeHighlighter from '@components/code-highlighter';
 import React, { memo } from 'react';
+import { getNameStrCode } from './code-examples';
 
 interface IProps {}
 
 const GetNameStr = ({}: IProps) => {
   return (
     <div className="ComponentRoot">
-      <h1>GetNameStr</h1>
+      <h1>getNameStr</h1>
+      <div>
+        Утилита, которая конкатенирует переданные строки через точку . Применимо для указания полей формы во вложенных
+        объектах.
+      </div>
+      <CodeHighlighter code={getNameStrCode} className="CodeHighlighter" />
     </div>
   );
 };
