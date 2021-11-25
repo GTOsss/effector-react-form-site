2065632384d23f1c620210064e8ace7ece12eadf
PavelMGs <pavelmg99@gmail.com>
2021-11-25 15:48:18 +0200

    setMeta

--- src/components/pages/unit-types/form/set-meta/set-meta.tsx
+++ src/components/pages/unit-types/form/set-meta/set-meta.tsx
@@ -1,11 +1,22 @@
+import CodeHighlighter from '@components/code-highlighter';
+import { Link } from 'gatsby-plugin-intl';
 import React, { memo } from 'react';
+import { metaType } from './code-examples';
 
 interface IProps {}
 
 const SetMeta = ({}: IProps) => {
   return (
     <div className="componentRoot">
-      <h1>SetMeta</h1>
+      <h1>setMeta</h1>
+      <div>
+        Принимает один аргумент, который будет записан в{' '}
+        <Link to="/api/unit-types/form/$meta" className="Link">
+          $meta
+        </Link>
+        .
+        <CodeHighlighter code={metaType} className="CodeHighlighter" />
+      </div>
     </div>
   );
 };
