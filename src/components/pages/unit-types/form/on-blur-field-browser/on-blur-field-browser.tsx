170b083ad1281d25ae2800d95e5247a0db17ae7f
PavelMGs <pavelmg99@gmail.com>
2021-11-25 17:03:03 +0200

    onBlurFieldBrowser

--- src/components/pages/unit-types/form/on-blur-field-browser/on-blur-field-browser.tsx
+++ src/components/pages/unit-types/form/on-blur-field-browser/on-blur-field-browser.tsx
@@ -6,6 +6,10 @@ const OnBlurFieldBrowser = ({}: IProps) => {
   return (
     <div className="ComponentRoot">
       <h1>OnBlurFieldBrowser</h1>
+      <div>
+        Событие, которые при потере фокуса на поле формы. На него можно подписаться через watch, sample и другие методы
+        API effector.
+      </div>
     </div>
   );
 };
