b54bc5ef7eef7eabccdc00541d7f04df2648aa6c
PavelMGs <pavelmg99@gmail.com>
2021-11-26 16:07:25 +0200

    useForm

--- src/components/pages/hooks/use-form/use-form.tsx
+++ src/components/pages/hooks/use-form/use-form.tsx
@@ -1,12 +1,52 @@
+import CodeHighlighter from '@components/code-highlighter';
+import { Link } from 'gatsby-plugin-intl';
 import React, { memo } from 'react';
-import styles from './styles.module.scss';
+import { useFormExample } from './code-examples';
+import UseFormExample from './example/use-form-example';
 
 interface IProps {}
 
 const UseForm = ({}: IProps) => {
   return (
     <div className="ComponentRoot">
-      <h1>UseForm</h1>
+      <h1>useForm</h1>
+      <div>
+        Хук для использования формы внутри функционального компонента. Принимает объект с полем form, meta и
+        resetUnmount.
+        <CodeHighlighter code={useFormExample} className="CodeHighlighter" />
+        Принцип работы вовзращаемых методов мы уже рассматривали выше, за исключением лишь controller и handleSubmit,
+        так что здесь мы поговорим о них.
+      </div>
+      <ol className="List">
+        <li>
+          <span className="ListItemName">controller</span>: функция, которая возвращает объект с методами для управления
+          полем формы. При вызове принимает объект с полями name, flat и validate.
+          <ul className="List">
+            <li>
+              <span className="ListItemName">name</span>: имя поля. Вложенные поля можно задавать с испльзованием{' '}
+              <Link to="/api/unit-types/form/get-name" className="Link">
+                getName
+              </Link>{' '}
+              и{' '}
+              <Link to="/api/unit-types/form/get-name-str" className="Link">
+                getNameStr
+              </Link>
+              .
+            </li>
+            <li>
+              <span className="ListItemName">flat</span>:
+            </li>
+            <li>
+              <span className="ListItemName">validate</span>: принимает функцию валидации для поля формы.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <span className="ListItemName">handleSubmit</span>: метод для передачи в обработчик onSubmit формы. Отменяет
+          действия по умолчанию события onSubmit.
+        </li>
+      </ol>
+      <UseFormExample />
     </div>
   );
 };
