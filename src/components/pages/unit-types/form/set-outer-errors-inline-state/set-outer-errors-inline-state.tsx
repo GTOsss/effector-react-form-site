334009f334bbaa6bb87facd8c2ba4afa9d454576
PavelMGs <pavelmg99@gmail.com>
2021-12-02 17:32:39 +0200

    setOuterErrorsInlineState

--- src/components/pages/unit-types/form/set-outer-errors-inline-state/set-outer-errors-inline-state.tsx
+++ src/components/pages/unit-types/form/set-outer-errors-inline-state/set-outer-errors-inline-state.tsx
@@ -1,27 +1,31 @@
 import CodeHighlighter from '@components/code-highlighter';
-import { Link } from 'gatsby-plugin-intl';
+import { FormattedMessage, Link } from 'gatsby-plugin-intl';
 import React, { memo } from 'react';
 import { ErrorsInlineType, setOuterErrorsInlineState } from './code-examples';
 
 interface IProps {}
+
+const path = 'components.pages.setOuterErrorsInlineState.';
 
 const SetOuterErrorsInlineState = ({}: IProps) => {
   return (
     <div className="ComponentRoot">
       <h1>setOuterErrorsInlineState</h1>
       <div>
-        Позволяет установить объект с внешними ошибками для полей формы. Является аналогом{' '}
+        <FormattedMessage id={`${path}description1st`} />{' '}
         <Link to="/api/unit-types/form/set-errors-inline-state" className="Link">
           setErrorsInlineState
         </Link>
-        , но для стора{' '}
+        , <FormattedMessage id={`${path}description2nd`} />{' '}
         <Link to="/api/unit-types/form/$outer-errors-inline" className="Link">
           $outerErrorsInline
         </Link>
         .
       </div>
       <CodeHighlighter code={ErrorsInlineType} className="CodeHighlighter" />
-      <h2>Пример использования</h2>
+      <h2>
+        <FormattedMessage id="components.all.usageExample" />
+      </h2>
       <CodeHighlighter code={setOuterErrorsInlineState} className="CodeHighlighter" />
     </div>
   );
