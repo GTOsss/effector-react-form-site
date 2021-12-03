9bd4f040a11066d53c8c8c3ac62b176be4cc0355
PavelMGs <pavelmg99@gmail.com>
2021-12-03 11:44:04 +0200

    getNameStr

--- src/components/pages/unit-types/form/get-name-str/get-name-str.tsx
+++ src/components/pages/unit-types/form/get-name-str/get-name-str.tsx
@@ -1,19 +1,22 @@
 import CodeHighlighter from '@components/code-highlighter';
-import { Link } from 'gatsby-plugin-intl';
+import { FormattedMessage, Link } from 'gatsby-plugin-intl';
 import React, { memo } from 'react';
 import { getNameStrCode } from './code-examples';
 
 interface IProps {}
+
+const path = 'components.pages.getNameStr.';
 
 const GetNameStr = ({}: IProps) => {
   return (
     <div className="ComponentRoot">
       <h1>getNameStr</h1>
       <div>
-        Утилита, которая конкатенирует переданные строки через точку. Поддерживает подсветку полей состояния, что
-        позволяет легче ориентироваться в больших объектах. Применимо для навигации по плоским объектам, таким как
-        состояние $fieldsInline В примере ниже, после открытия кавычек в form.getNameStr(), предложит варианты полей. В
-        ts/tsx файлах, при указании несуществующих полей, покажет ошибку типизации.
+        <FormattedMessage id={`${path}description1st`} />{' '}
+        <Link to="/api/unit-types/form/$fields-inline/" className="Link">
+          $fieldsInline
+        </Link>
+        <FormattedMessage id={`${path}description2nd`} />
       </div>
       <CodeHighlighter code={getNameStrCode} className="CodeHighlighter" />
     </div>
