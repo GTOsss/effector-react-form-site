e5ca4940256ead1a2947ede30f75dfa82bca6556
PavelMGs <pavelmg99@gmail.com>
2022-01-18 13:26:08 +0200

    deps

---
+++ src/components/pages/main/code-example/simple-form/index.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { createForm, useForm } from 'effector-react-form';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+
+const form = createForm({
+  onSubmit: ({ values }) => alert(`Hi there, ${values.username}`),
+});
+
+const validateRequired = (value) => (!value ? 'Field is required' : undefined);
+
+const Input = ({ controller, label }) => {
+  const { input, error, isShowError } = controller();
+  return (
+    <div className="input-wrap">
+      <label>{label}</label>
+      <input
+        {...input}
+        value={input.value || ''}
+        className={cn('input', {
+          'input-error': isShowError,
+        })}
+        autoComplete="off"
+      />
+      {isShowError && <div className="input-error-message">{error}</div>}
+    </div>
+  );
+};
+
+const SimpleForm = () => {
+  const { controller, handleSubmit, submit } = useForm({ form });
+  return (
+    <div className={styles.root}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <Input controller={controller({ name: 'username', validate: validateRequired })} label="Username" />
+        <Input controller={controller({ name: 'firstName' })} label="First name" />
+        <Input controller={controller({ name: 'lastName' })} label="Last name" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
+
+export const SimpleFormCode = `import React from 'react';
+import { createForm, useForm } from 'effector-react-form';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+
+const form = createForm({
+  onSubmit: ({ values }) => alert('Hi there, ' + values.username),
+});
+
+const validateRequired = (value) => (!value ? 'Field is required' : undefined);
+
+const SimpleForm = () => {
+  const { controller, handleSubmit } = useForm({ form });
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <Input controller={controller({ name: 'username', validate: validateRequired })} label="Username" />
+      <Input controller={controller({ name: 'firstName' })} label="First name" />
+      <Input controller={controller({ name: 'lastName' })} label="Last name" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+const Input = ({ controller, label }) => {
+  const { input, error, isShowError } = controller();
+  return (
+    <div className="input-wrap">
+      <label>{label}</label>
+      <input
+        {...input}
+        value={input.value || ''}
+        className={cn('input', {
+          'input-error': isShowError,
+        })}
+        autoComplete="off"
+      />
+      {isShowError && <div className="input-error-message">{error}</div>}
+    </div>
+  );
+};
+
+export default SimpleForm;
+`;
